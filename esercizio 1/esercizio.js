e4d57d48c6dc436479e4370011d1070f70821c8d
Emanuele <emanuele.nannizzi@hotmail.it>
2025-03-03 12:04:15 +0100

    sconti.js

--- esercizio 1/esercizio.js
+++ esercizio 1/esercizio.js
@@ -1,2 +1,33 @@
-\\ 1. Dichiara alcune variabili di tipo diverso
-let nomeStudente
+// 1. Dichiara alcune variabili di tipo
+diverso
+let nomeStudente = "Mario";
+let etaStudente = 20;
+let corsoAttivo = true; // Booleano
+
+// 2. Stampa in console
+console.log( Il nome dello studente è:" ,
+nomeStudente );
+console.log( L'età dello studente è:"  ,
+etaStudente);
+console.log( Lo studente è iscritto al 
+corso? , corsoAttivo );
+
+// 3. Esegui alcune operazioni
+matematiche
+let numero1 = 10;
+let numero2 = 3;
+let risultatoSomma = numero1 + numero2;
+let risultatoSottrazione = numero1 - 
+numero2;
+let risultatoMoltiplicazione = numero1 *
+numero2;
+let risultatoDivisione = numero1 /
+numero2;
+
+console.log( "Somma:" risultatoSomma );
+console.log( "Sottrazione:" ,
+risultatoSottrazione );
+console.log ("Moltiplicazione:",
+risultatoMoltiplicazione ) ;
+console.log( "Divisione:" ,
+risultatoDivisione ) ;
