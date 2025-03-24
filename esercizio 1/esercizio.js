16db9f9c779e9a7eb971cfda235adbf3ea98b48c
Emanuele <emanuele.nannizzi@hotmail.it>
2025-03-24 12:17:36 +0100

    esercizio 1

--- esercizio 1/esercizio.js
+++ esercizio 1/esercizio.js
@@ -5,29 +5,34 @@ let etaStudente = 20;
 let corsoAttivo = true; // Booleano
 
 // 2. Stampa in console
-console.log( Il nome dello studente è:" ,
-nomeStudente );
-console.log( L'età dello studente è:"  ,
-etaStudente);
-console.log( Lo studente è iscritto al 
-corso? , corsoAttivo );
+console.log( "Il nome dello studente è:" , nomeStudente ) ;
+console.log( "L'età dello studente è:"  , etaStudente);
+console.log( "Lo studente è iscritto al corso? ", corsoAttivo );
 
 // 3. Esegui alcune operazioni
 matematiche
 let numero1 = 10;
 let numero2 = 3;
 let risultatoSomma = numero1 + numero2;
-let risultatoSottrazione = numero1 - 
-numero2;
-let risultatoMoltiplicazione = numero1 *
-numero2;
-let risultatoDivisione = numero1 /
-numero2;
+let risultatoSottrazione = numero1 - numero2;
+let risultatoMoltiplicazione = numero1 * numero2;
+let risultatoDivisione = numero1 / numero2;
 
-console.log( "Somma:" risultatoSomma );
+console.log("Somma:", risultatoSomma ) ;
 console.log( "Sottrazione:" ,
 risultatoSottrazione );
-console.log ("Moltiplicazione:",
-risultatoMoltiplicazione ) ;
-console.log( "Divisione:" ,
-risultatoDivisione ) ;
+console.log ("Moltiplicazione:", risultatoMoltiplicazione ) ;
+console.log( "Divisione:" , risultatoDivisione ) ;
+
+// 4. Concatenazione di stringhe
+let primaParte = "Ciao, io sono Emanuele " ;
+let secondaParte = "e ho 25 " ;
+let terzaParte = " 25 anni " ;
+
+let fraseCompleta = primaParte + nomeStudente + secondaParte + etaStudente + terzaParte;
+console.log ( fraseCompleta ) ;
+
+// 5. Prova un prompt per chiedere un valore all'utente ( opzionale)
+// Apri la pagine in un browser e osserva il comportamento
+let colorePreferito = prompt( "Qual è il tuo colore preferito? ") ;
+console.log( "Il colore preferito inserito è . blu" , colorePreferito) ;
